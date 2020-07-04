66eb2a45e7968076eafcfbdbaac9cfbb6dc256b0
Ubuntu <ubuntu@ip-172-31-1-214.ap-northeast-1.compute.internal>
2020-07-04 07:41:08 +0000

    [Add] User create and update

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  get 'users' => 'users#index'
+  get 'users/:id' => 'users#show'
+  get 'signup' => 'users#new'
+  post 'signup' => 'users#create'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/:id/update' => 'users#update'
+  get 'login' => 'users#login_form'
+  post 'login' => 'users#login'
+  
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
+  get 'posts' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  post 'posts' => 'posts#create'
+  get 'posts/:id' => 'posts#show'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
